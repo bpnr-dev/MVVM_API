800e78551952c11a8d12a46e9ca75acf9bf122bc
Guilherme Carvalho <guilherme.santos536@etec.sp.gov.br>
2023-11-06 02:41:09 -0300

    Adicionada classe ToDo

---
+++ ViewModels/TodoViewModel.cs
@@ -0,0 +1,25 @@
+﻿using MVVM_API_SampleProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_API_SampleProject.ViewModels
+{
+    public class TodoViewModel
+    {
+        public Todo Todo { get; set; }
+
+        public TodoViewModel()
+        {
+            Todo = new Todo()
+            {
+                UserId = 1,
+                Id = 1,
+                Title = "Titulo da Tarefa",
+                Completed = false
+            };
+        }
+    }
+}
---
+++ Views/TodoView.xaml.cs
@@ -0,0 +1,9 @@
+namespace MVVM_API_SampleProject.Views;
+
+public partial class TodoView : ContentPage
+{
+	public TodoView()
+	{
+		InitializeComponent();
+	}
+}
